8f2f8d544eaefb5d8adf35d07c944cab08c60b7f
LuizJack <lcamargopolo@gmail.com>
2023-05-28 14:48:35 -0500

    init commit

---
+++ testServices/testServicesApp.swift
@@ -0,0 +1,17 @@
+//
+//  testServicesApp.swift
+//  testServices
+//
+//  Created by Luiz Camargo on 28/05/23.
+//
+
+import SwiftUI
+
+@main
+struct testServicesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
