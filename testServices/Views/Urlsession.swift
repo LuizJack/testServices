baf9ef872a3c5d08fc0a1a32cc059ba801a2e618
LuizJack <lcamargopolo@gmail.com>
2023-05-28 15:14:14 -0500

    add url session view

---
+++ testServices/Views/Urlsession.swift
@@ -0,0 +1,20 @@
+//
+//  Urlsession.swift
+//  testServices
+//
+//  Created by Luiz Camargo on 28/05/23.
+//
+
+import SwiftUI
+
+struct Urlsession: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Urlsession_Previews: PreviewProvider {
+    static var previews: some View {
+        Urlsession()
+    }
+}
