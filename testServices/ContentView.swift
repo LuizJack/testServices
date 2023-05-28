eb957baa40f89eb7f63832198edc0652739f0053
LuizJack <lcamargopolo@gmail.com>
2023-05-28 14:53:04 -0500

    commit 3

--- testServices/ContentView.swift
+++ testServices/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Cambios")
+            Text("Cambios nuevos")
         }
         .padding()
     }
