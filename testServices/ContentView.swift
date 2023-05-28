bdfaa54da7d1b901f0c28319bb13ced56fa5beab
LuizJack <lcamargopolo@gmail.com>
2023-05-28 15:12:02 -0500

    config init

--- testServices/ContentView.swift
+++ testServices/ContentView.swift
@@ -9,16 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Cambios nuevos !!")
+        Text("Test services")
+            .font(.largeTitle)
+            .foregroundColor(.black)
+        NavigationView {
+            
+            VStack(spacing: 10) {
+                
+                Image(systemName: "pc")
+                    .resizable()
+                    .foregroundColor(.red)
+                    .frame(width: 100, height: 100)
+                    .shadow(radius: 4)
+                
+                
+                List {
+                    Group {
+                        //                        NavigationLink(destination: GPSView() ) {
+                        //                            VStack(alignment: .leading, spacing: 20 ) {
+                        //                                  HStack {
+                        //                                      Text("GPS").font(.subheadline)
+                        //                                      Spacer()
+                        //                                      Text("Test").font(.subheadline)
+                        //                                  }
+                        //                            }
+                        //                        }
+                        
+                        
+                    } // group
+                }
+                .listStyle(.plain)
+                
+            }
         }
-        .padding()
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
